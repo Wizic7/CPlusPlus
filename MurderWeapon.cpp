8bd6adbac42a0f6bd8cc34a0a3354351b585fb6b
Joseph Robles <rjosepha@byui.edu>
2025-01-30 21:18:46 -0700

    Added some item descriptions as well as set up child classes

--- MurderWeapon.cpp
+++ MurderWeapon.cpp
@@ -19,13 +19,19 @@ Dynamite
 
 class MurderWeapon {
     
-    const std::array<std::string, 5> potentialMurderWeapons = {"Club", "Sharp Stick", "White Gloves", "Worn Rope", "Bad Fish"};
+    const std::array<std::string, 5> potentialMurderWeaponDescriptions = {
+    "This item was once used to swat flies, but one day it swatted more than just a bug.",
+    "This item was a toothpick for a giant, until it found a new purpose.",
+    "This item was pristine until it got involved in a messy situation.",
+    "This item was used in a tug-of-war game that ended in a surprising twist.",
+    "This item was so smelly, it could knock out anyone who came too close.",
+    };
 
     public: 
 
     //Attributes
     std::string murderWeaponName;
-    std::string weaponLocation; //Where will the weapon be in the game
+    std::string weaponLocation = "Hello World"; //Where will the weapon be in the game
     std::string murderWeaponDescription; //GRAB STRING FROM AN ARRAY OF POTENTIAL DESCRIPIONS 
 
     //Constructors
@@ -38,3 +44,34 @@ class MurderWeapon {
     }
 
 }; 
+
+
+class Club : public MurderWeapon {
+
+};
+
+class SharpStick : public MurderWeapon {
+
+};
+
+class WhiteGlove : public MurderWeapon {
+
+};
+
+class WornRope : public MurderWeapon {
+
+};
+
+class RottenFish : public MurderWeapon {
+
+};
+
+
+int main(){ //example main function for testing purposes.
+    RottenFish poison;
+    
+    std::string hello = poison.returnLocation(); 
+    
+    std::cout << hello << std::endl;
+
+}
