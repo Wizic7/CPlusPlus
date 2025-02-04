8785cb0fc3f32598bcf078ba46110ee9860c6786
Tsuru4 <cody7ac@gmail.com>
2025-02-04 13:24:15 -0700

    correcting file name issues with Clue and Player class files also (partially) corrected namespace issues with Player

--- Clue.cpp
+++ Clue.cpp
@@ -1,6 +1,14 @@
 // * For questions on this class, see Allyn
 #include <string>
 
+/*
+Clue
+
+Parameters = [
+Description
+];
+*/
+
 class Clue {
     public:
     std::string clueDescription = "Useless clue. See Allyn if this appears.";
