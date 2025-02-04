710ab0778cb8478119c9a12c87b1b9b7ef09cf96
Tsuru4 <cody7ac@gmail.com>
2025-02-04 13:51:59 -0700

    corrected some minor errors and spacing conventions in Clue and Player

--- Player.cpp
+++ Player.cpp
@@ -4,26 +4,26 @@
 
 class Player {
 // TODO player stats needed
-    public:
+public:
     
     std::string playerName = "Ooka Ooka";
     // ? The planning documentation says we want to implement a player inventory, but it is unclear what we are going to be putting in the inventory. We need to rediscuss this so I know how to implement it.
 
     // TODO the size of this array will need to be adjusted later. It is currently set to 3, but it we will need expanded as we decide the specifics of the game.
-    int journalSize = 3;
-    Clue *clueJournal = new Clue[journalSize];
 
     Player() {}
+/*
+private:
+    void initializeJournal()
+    {
+        for (int i = 0; i < journalSize; i++)
+        {
+            clueJournal[i] = clueJournal(); // TODO this will need to be updated with specific parameters later on.
+        }
+    }*/
 };
 
-private void initialize()
-{
-    
-    for (int i = 0; i < journalSize; i++)
-    {
-        clueJournal[i] = clueJournal(); // TODO this will need to be updated with specific parameters later on.
-    }
-}
+
     // Sources:
     // Google's AI was used to refamiliarzie myself with the basic syntax for class declaration, constructers and member functions in C++.
     // https://www.geeksforgeeks.org/how-to-initialize-array-of-objects-with-parameterized-constructors-in-c/
