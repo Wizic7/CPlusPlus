a9620c80a2f83fcf71aa58e8ebf1ca44aa50c654
Kaiharal <kaiharalofthejadetower@gmail.com>
2025-02-05 17:13:40 -0700

    Creating NPC class
    adding in a basic structure for the NPC class. Will be added to over time.

---
+++ NPC.cpp
@@ -0,0 +1,18 @@
+//For questions on this class, see Mason
+#include <string>;
+#include <Clue.cpp>;
+#include <list>;
+
+
+class NPC {
+
+public: 
+	
+	std::string npcName = "placeholder name";
+	std::string profession = "placeholder profession"
+	int age;
+	std::list<Clue> relatedClues; //not sure how we're going to relate clues to NPCs yet, but this is here for now.
+	//We mentioned doing some random backstories, but i think that the backstory generation might be its own thing entirely.
+
+	//there's obviously going to be more methods here eventually for getting data from the class, but we don't know what's going to be needed yet.
+};
