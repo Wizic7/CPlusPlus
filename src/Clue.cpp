d76498ea635bcb30e7075a724504ccb4de5f76ab
jrgauntjr <joseph.r.gaunt@gmail.com>
2025-02-11 13:34:17 -0700

    change
    change

---
+++ src/Clue.cpp
@@ -0,0 +1,17 @@
+// * For questions on this class, see Allyn
+#include <string>
+
+/*
+Clue
+
+Parameters = [
+Description
+];
+*/
+
+class Clue {
+public:
+    std::string clueDescription = "Useless clue. See Allyn if this appears.";
+    Clue() {}
+
+};
