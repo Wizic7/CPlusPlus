9bb267942149e1c7bfe3df2213dcb1381c23ddc8
jrgauntjr <joseph.r.gaunt@gmail.com>
2025-02-19 15:10:06 -0700

    Got the main.cpp file runing and in working condition.

--- src/NPC.cpp
+++ src/NPC.cpp
@@ -1,6 +1,6 @@
 //For questions on this class, see Mason
 #include <string>;
-#include <Clue.cpp>;
+//#include <Handler/Clue.cpp>;
 #include <list>;
 
 
@@ -8,10 +8,10 @@ class NPC {
 
 public: 
 	
-	std::string npcName = "placeholder name";
-	std::string profession = "placeholder profession"
-	int age;
-	std::list<Clue> relatedClues; //not sure how we're going to relate clues to NPCs yet, but this is here for now.
+	//std::string npcName = "placeholder name";
+	//std::string profession = "placeholder profession";
+	//int age;
+	//std::list<Clue> relatedClues; //not sure how we're going to relate clues to NPCs yet, but this is here for now.
 	//We mentioned doing some random backstories, but i think that the backstory generation might be its own thing entirely.
 
 	//there's obviously going to be more methods here eventually for getting data from the class, but we don't know what's going to be needed yet.
