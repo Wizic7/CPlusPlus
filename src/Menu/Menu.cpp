733526cb00faa5bb7cd1b618f27754d1be688491
Isaac Cope <IsaacWIzic7@gmail.com>
2025-03-25 13:50:39 -0600

    Main Menu (minus images)

--- src/Menu/Menu.cpp
+++ src/Menu/Menu.cpp
@@ -1,5 +1,8 @@
 #include "Menu.h"
 #include "raylib.h"
+#include "array"
+#include <vector>
+
 
 
 void Menu::Draw()
@@ -10,28 +13,77 @@ void Menu::Draw()
 		int spacing = GetScreenHeight() * .05;
 		int button_x = buttonWidth / 2;
 		int button_y = buttonHeight / 3;
+		
+		for(int i = 0; i < buttonText.size(); i++){
+			int x_pos = GetScreenWidth() / 2 - buttonWidth / 2;
+			int y_pos = GetScreenHeight() - spacing * buttonCount - buttonHeight * buttonCount;
+			const char* text = buttonText[i].c_str();
 
-		int x_pos = GetScreenWidth() / 2 - buttonWidth / 2;
-		int y_pos = GetScreenHeight() - spacing * buttonCount - buttonHeight * buttonCount;
-		DrawRectangle(x_pos, y_pos, buttonWidth, buttonHeight, GRAY);
-		DrawText("Settings", x_pos + (button_x - MeasureText("Settings", fontSize)/2), y_pos + button_y, fontSize, RED);
-		buttonCount++;
-		x_pos = GetScreenWidth() / 2 - buttonWidth / 2;
-		y_pos = GetScreenHeight() - spacing * buttonCount - buttonHeight * buttonCount;
-		DrawRectangle(x_pos, y_pos, buttonWidth, buttonHeight, GRAY);
-		DrawText("Load Game", x_pos + (button_x - MeasureText("Load Game", fontSize) / 2), y_pos + button_y, fontSize, RED);
-		buttonCount++;
-		x_pos = GetScreenWidth() / 2 - buttonWidth / 2;
-		y_pos = GetScreenHeight() - spacing * buttonCount - buttonHeight * buttonCount;
-		DrawRectangle(x_pos, y_pos, buttonWidth, buttonHeight, GRAY);
-		DrawText("New Game", x_pos + (button_x - MeasureText("New Game", fontSize) / 2), y_pos + button_y, fontSize, RED);
-		buttonCount++;
+			Color recColor = GRAY;
+			Color textColor = RED;
+			if (hovered == i)
+			{
+				recColor = BLUE;
+				textColor = WHITE;
+			}
+			
+			buttonYPos[buttonCount - 1] = y_pos;
+			buttonXPos = x_pos;
+
+			DrawRectangle(x_pos, y_pos, buttonWidth, buttonHeight, recColor);
+			DrawText(text, x_pos + (button_x - MeasureText(text, fontSize) / 2), y_pos + button_y, fontSize, textColor);
+			buttonCount++;
+		}
 	}
 
 }
 
 void Menu::Update()
 {
+	int mouseX = GetMouseX();
+	int mouseY = GetMouseY();
+	bool inXRange = mouseX > buttonXPos && mouseX < buttonXPos + buttonWidth;
+	bool inYRange = false;
+
+	if (inXRange) {
+		
+		for (int i = 0; i < buttonYPos.size(); i++)
+		{
+			if (mouseY > buttonYPos[i] && mouseY < buttonYPos[i] + buttonHeight)
+			{
+				hovered = i;
+				inYRange = true;
+			}
+		}
+	}
+	if(!inXRange || !inYRange)
+	{
+		hovered = -1;
+	}
+
+	if (IsMouseButtonReleased)
+	{
+		if (hovered >= 0)
+		{
+			switch (hovered) {
+				case 0:
+				{
+				//Settings
+					break;
+				}
+				case 1:
+				{
+					//Load game
+					break;
+				}
+				case 2:
+				{
+					//New game
+					break;
+				}
+			}
+		}
+	}
 
 }
 
--- src/Menu/Menu.h
+++ src/Menu/Menu.h
@@ -1,11 +1,19 @@
 #pragma once
 #include "raylib.h"
+#include "array"
+#include <string>
+#include <vector>
 class Menu
 {
-private: 
+private:
 	int buttonWidth = GetScreenWidth() * 0.35;
 	int buttonHeight = GetScreenHeight() * 0.15;
 	int fontSize = 36;
+	int hovered = -1;
+	int buttonXPos;
+	std::array<int,3> buttonYPos;
+	std::vector<std::string> buttonText = {"Settings", "Load Game", "New Game"};
+
 	bool isLoaded = false;
 
 
